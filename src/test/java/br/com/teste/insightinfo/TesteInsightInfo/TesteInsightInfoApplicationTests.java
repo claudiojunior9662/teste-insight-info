ea741b80e0f1056a073f8c589d9276b084b49700
claudiojunior9662 <claudiojunior9662@gmail.com>
2023-05-12 16:49:31 -0300

    [ADD] inital project

---
+++ src/test/java/br/com/teste/insightinfo/TesteInsightInfo/TesteInsightInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.insightinfo.TesteInsightInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteInsightInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
